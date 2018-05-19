9a12ae9ab228e3110f5d9b78f8c0106d456083a0
Alex Wong <alexwongsenhung@gmail.com>
2018-05-18 18:00:51 -0700

    Model Structure

---
+++ jsondb/Model/Model.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+
+
+class Model(object):
+    def __init__(self, primary=None):
+        pass
+
+    def save(self):
+        pass
+
+    def add_relation(self):
+        pass
--- jsondb/__init__.py
+++ jsondb/__init__.py
@@ -1,1 +1,2 @@
 from Database.Database import Database as DB
+from Model.Model import Model
