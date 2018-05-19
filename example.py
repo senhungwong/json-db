988ea466987de6b8d554987d596ddebe30695951
Alex Wong <alexwongsenhung@gmail.com>
2018-05-18 21:51:30 -0700

    Model Initializaion

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from jsondb import DB
+from jsondb import DB, Model
 
 # create a database object having database name 'example_db' under example storage
 jsondb = DB('example_db', 'example')
@@ -8,3 +8,23 @@ jsondb.init()
 
 # create type
 jsondb.create_type('user')
+
+
+# create user model
+class User(Model):
+    """User type model.
+
+    Attributes:
+        email (str): The email address of user.
+    """
+
+    storage = 'example'
+    database = 'example_db'
+    type = 'users'
+
+
+# find user who has primary 'alex'
+user = User('alex')
+
+# get user's email
+print user.email
--- jsondb/Model/Model.py
+++ jsondb/Model/Model.py
@@ -1,9 +1,49 @@
 # -*- coding: UTF-8 -*-
+from ..FileManager.FileManager import FileManager
 
 
 class Model(object):
+    storage = 'jsondb'
+
+    database = None
+
+    type = None
+
     def __init__(self, primary=None):
-        pass
+        """Initialize object.
+
+        If primary is not given, new type object is created. If primary is given,
+        try to find the .json file in storage and assign attributes to the current
+        object.
+
+        Args:
+            primary (str): If given, search data; otherwise create new object.
+        """
+
+        # init file manager
+        self.file_manager = FileManager(self.storage)
+
+        # create new object if primary is not given
+        if primary is None:
+            self.__dict__ = {}
+            return
+
+        # model should have basic storing information
+        if not self.storage or not self.database or not self.type:
+            exit('Model %s/%s/%s should be declared in class.' % (self.storage, self.database, self.type))
+
+        # make path
+        path = self.database + '/data/' + self.type + '/' + primary + '.json'
+
+        # check if path exists
+        if not self.file_manager.exists(path):
+            exit('Path %s is not found.' % path)
+
+        # set attributes from .json file
+        self.__dict__ = self.file_manager.read(path)
+
+        # set current object primary
+        self.primary = primary
 
     def save(self):
         pass
